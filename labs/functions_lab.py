d045eb0375b48f11a5c1ae07310950570628ff8f
Heather Creighton <heather.creighton@gmail.com>
2022-06-02 20:07:51 +0000

    Initial commit

---
+++ labs/functions_lab.py
@@ -0,0 +1,25 @@
+# **Exercise 1**
+# Create a function called age_calculator that takes in two parameters (Two different years) and returns the difference of the two years:
+
+def age_calculator(year1, year2):
+  # TODO: add your code below. Remove the word "pass" when complete 
+
+  pass
+
+
+print(age_calculator(2018, 1900))  #output to terminal should be 118
+
+
+
+
+
+# **Exercise 2**
+# Define another function, avg_numbers that takes three parameters, that returns the average of three numbers.
+
+
+def avg_numbers(x, y, z):
+  # TODO: add your code below. Remove the word "pass" when complete 
+
+  pass
+
+print(avg_numbers(4, 7, 8)) #output should be 6.33333333
